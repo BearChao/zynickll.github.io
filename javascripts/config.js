8ae259ee434e896819e409f57be2036263f4e93e
zynickLL <zynick@foxmail.com>
2015-01-26 14:11:44 +0800

    Update config.js

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,8 +1,8 @@
-var disqus_shortname = 'hooloodemo';			//Short Name of disqus, this parameter is for disqus comment box.
-var hostbase = 'http://hooloo.github.io';		//The absolute root address of your blog without '/' in the end.
-var githubname = 'hooloo';						//Your username of GitHub.
-var repos = 'hooloo.github.io';					//Name of repository that contains your blog.
-var sitetitle = 'Hooloo';						//Name of your blog.
+var disqus_shortname = 'define';			//Short Name of disqus, this parameter is for disqus comment box.
+var hostbase = 'http://blog.nickzy.com';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'zynickLL';						//Your username of GitHub.
+var repos = 'zynickll.github.io';					//Name of repository that contains your blog.
+var sitetitle = 'define';						//Name of your blog.
 var rss = '';									//Rss address of your blog.
 var suffix = '';								//Suffix of post files, '.' should be included.
 var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
