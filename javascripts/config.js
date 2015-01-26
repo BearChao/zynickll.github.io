0dc13e7f7cf0e3c06a15e6443aa527a8e4b5d78c
zynickll <zynick@foxmail.com>
2015-01-26 14:08:15 +0800

    test

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,8 +1,18 @@
-var disqus_shortname = 'define';			//Short Name of disqus, this parameter is for disqus comment box.
-var hostbase = 'http://blog.nickzy.com';		//The absolute root address of your blog without '/' in the end.
-var githubname = 'zynickLL';						//Your username of GitHub.
-var repos = 'zynickll.github.io';					//Name of repository that contains your blog.
-var sitetitle = 'define's blog';						//Name of your blog.
+var disqus_shortname = 'hooloodemo';			//Short Name of disqus, this parameter is for disqus comment box.
+var hostbase = 'http://hooloo.github.io';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'hooloo';						//Your username of GitHub.
+var repos = 'hooloo.github.io';					//Name of repository that contains your blog.
+var sitetitle = 'Hooloo';						//Name of your blog.
 var rss = '';									//Rss address of your blog.
 var suffix = '';								//Suffix of post files, '.' should be included.
 var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
+
+//Sample Config:
+//var disqus_shortname = 'sneezry';
+//var hostbase = 'http://sneezry.com/blog';
+//var githubname = 'sneezry';
+//var repos = 'blog';	
+//var sitetitle = 'Sneezry';
+//var rss = '';
+//var suffix = '.md';
+//var branch = 'gh-pages';
